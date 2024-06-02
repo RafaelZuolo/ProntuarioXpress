b73fc62ea2b69aed87e24769e44a8d322b6c4844
Rafael Zuolo Coppini Lima <rafaelzcl@gmail.com>
2024-06-01 23:33:07 -0300

    Added CoreTests Project and fixed GetMonths issue

--- Core/Utils/DateTimeExtensions.cs
+++ Core/Utils/DateTimeExtensions.cs
@@ -3,10 +3,11 @@
 public static class DateTimeExtensions
 {
     private const double NumberOfDaysInYear = 365.2425;
+    private const double NumberOfDaysInMonth = 30.4167;
 
     public static int GetYearsFromToday(this DateTime birthDay) =>
         (int)(DateTime.Today.Subtract(birthDay).TotalDays / NumberOfDaysInYear);
 
     public static int GetMonthsFromToday(this DateTime birthDay) =>
-        (int)(DateTime.Today.Subtract(birthDay).TotalDays / NumberOfDaysInYear);
+        (int)(DateTime.Today.Subtract(birthDay).TotalDays / NumberOfDaysInMonth);
 }
